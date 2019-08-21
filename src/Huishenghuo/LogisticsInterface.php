6852da07cc747be19cff64e4ff265369eb8c9c3b
15511603197 <15511603197>
2019-08-21 18:56:16 +0800

    1.1.1

---
+++ src/Huishenghuo/LogisticsInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Huishenghuo;
+
+/**
+ * Interface LogisticsInterface
+ * @package Huishenghuo
+ */
+interface LogisticsInterface
+{
+    public function getLogistics(array $arr);
+}
