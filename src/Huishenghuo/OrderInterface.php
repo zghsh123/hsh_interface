b48364b20a19f46993e2d137cc624081e4863540
15511603197 <15511603197>
2019-08-21 19:53:00 +0800

    1.1.3

--- src/Huishenghuo/OrderInterface.php
+++ src/Huishenghuo/OrderInterface.php
@@ -35,25 +35,28 @@ interface OrderInterface
      * email:3081089511@qq.com
      * Created by fhc on 2019-08-19-13:19
      * desc:支付成功订单
+     * @param array $arr
      * @return mixed
      */
-    public function pay_success_order();
+    public function pay_success_order(array $arr);
 
     /**
      * author:fhc
      * email:3081089511@qq.com
      * Created by fhc on 2019-08-19-13:19
      * desc:完全关闭订单
+     * @param array $arr
      * @return mixed
      */
-    public function closed_order();
+    public function closed_order(array $arr);
 
     /**
      * author:fhc
      * email:3081089511@qq.com
      * Created by fhc on 2019-08-21-19:49
      * desc:获取订单物流
+     * @param array $arr
      * @return mixed
      */
-    public function getOrderLogists();
+    public function getOrderLogists(array $arr);
 }
