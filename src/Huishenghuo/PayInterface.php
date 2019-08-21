295bb9d631c42008275fbb4cf92f786d38d51794
15511603197 <15511603197>
2019-08-21 18:43:12 +0800

    定义接口基础文件

--- src/Huishenghuo/PayInterface.php
+++ src/Huishenghuo/PayInterface.php
@@ -15,7 +15,28 @@ interface PayInterface
      * email:3081089511@qq.com
      * Created by fhc on 2019-08-19-12:00
      * desc:方法说明
+     * @param array $arr
      * @return mixed
      */
-    public function getPay();
+    public function getPay(array $arr);
+
+    /**
+     * author:fhc
+     * email:3081089511@qq.com
+     * Created by fhc on 2019-08-21-18:42
+     * desc:退款
+     * @param array $arr
+     * @return mixed
+     */
+    public function orderRefund(array $arr);
+
+    /**
+     * author:fhc
+     * email:3081089511@qq.com
+     * Created by fhc on 2019-08-21-18:42
+     * desc:方法说明
+     * @param array $arr
+     * @return mixed
+     */
+    public function orderSelectResult(array $arr);
 }
