e6636a2c6836d8744c8bd529a87fcc39ebb3a1b9
15511603197 <15511603197>
2019-09-16 17:41:15 +0800

    定义接口基础文件

---
+++ src/Huishenghuo/TianShiInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Huishenghuo;
+
+/**
+ * author:fhc
+ * email:3081089511@qq.com
+ * Created by fhc on 2019-09-16-17:39
+ * desc:天时同城第三方
+ * Class TianShiInterface
+ * @package Huishenghuo
+ */
+interface TianShiInterface
+{
+    /**
+     * author:fhc
+     * email:3081089511@qq.com
+     * Created by fhc on 2019-09-16-17:39
+     * desc:获取商品数据
+     * @param array $data
+     * @return mixed
+     */
+    public function getGoodsList(array $data);
+
+    /**
+     * author:fhc
+     * email:3081089511@qq.com
+     * Created by fhc on 2019-09-16-17:40
+     * desc:支付成功下取订单
+     * @param array $data
+     * @return mixed
+     */
+    public function addOrder(array $data);
+}
